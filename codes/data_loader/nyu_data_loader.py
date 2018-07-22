7f71fcc5cbbc4c5988917495ab365300cd9deb73
Y2P <yigit.pilavci01@gmail.com>
2018-07-22 16:38:31 +0200

    Loss Functions

--- codes/data_loader/nyu_data_loader.py
+++ codes/data_loader/nyu_data_loader.py
@@ -1,36 +1,110 @@
 import numpy as np
 import h5py
+import tensorflow as tf
+import random
+import os
+import pickle
+
+
+def _resize_data(image, depthmap):
+    """Resizes images to smaller dimensions."""
+    image = tf.image.resize_images(image, [48, 64])
+    depthmap = tf.image.resize_images(depthmap, [24, 32])
+
+    return image, depthmap
+
+def _flip_left_right(image, depthmap):
+    """Randomly flips image and depth_map left or right in accord."""
+    seed = random.random()
+    image = tf.image.random_flip_left_right(image, seed=seed)
+    depth_map = tf.image.random_flip_left_right(depthmap, seed=seed)
+
+    return image, depth_map
+
+
+def _normalize_data(image, depthmap):
+    """Normalize image and depth_map within range 0-1."""
+    image = tf.cast(image, tf.float32)
+    #image = image / 255.0
+
+    depthmap = tf.cast(depthmap, tf.float32)
+    #depthmap = depthmap / 255.0
+    #depthmap = tf.cast(depthmap, tf.int32)
+
+    return image, depthmap
+
+
+def _parse_depthmap(image_path, depthmap_path):
+
+    fp = open(depthmap_path, 'rb')
+    depthmap = pickle.load(fp)
+    # depthmap = np.load(depthmap_path)
+    depthmap = depthmap.astype(np.float32)
+    depthmap = np.expand_dims(depthmap, axis=-1)
+    # fp.close()
+
+    return image_path, depthmap
+
+
+def _parse_image(image_path, depthmap):
+
+    """Reads image and depth_map files"""
+    image_content = tf.read_file(image_path)
+    image = tf.image.decode_png(image_content, channels=3)
+
+    return image, depthmap
 
 
 class NYUDataLoader:
-	def __init__(self, config):
-		self.config = config
-		print("Loading the data")
-		# Large .mat file can be loaded only by h5py
-		f = h5py.File(self.config.data_path)
-		# Load input images
-		self.input = np.array(list(f['images']))
+    def __init__(self, config):
+        self.config = config
+        print("Loading the data")
+        image_folder = self.config.image_folder
+        depthmap_folder = self.config.depthmap_folder
+        self.augment = self.config.augment
+        self.batch_size = self.config.batch_size
+        self.num_threads = self.config.num_threads
 
-		numofimages = self.input.shape[0]
+        self.image_paths = sorted([os.path.join(image_folder, x) for x in os.listdir(image_folder) if x.endswith('.jpeg')])
+        self.depthmap_paths = sorted([os.path.join(depthmap_folder, x) for x in os.listdir(depthmap_folder) if x.endswith('.pkl')])
 
-		# Reshaping for network
-		self.input = np.reshape(self.input,[self.input.shape[0],self.input.shape[2],self.input.shape[3],self.input.shape[1]])		
-		# Load output images
-		self.y = np.array(list(f['depths']))
-		# Reshaping for network		
-		self.y = np.reshape(self.y,[self.y.shape[0],self.y.shape[1],self.y.shape[2],1])		
+        images_name_tensor = tf.constant(self.image_paths)
+        mask_name_tensor = tf.constant(self.depthmap_paths)
 
-		# Index list of unused images by batch
-		self.batch_list = np.array(range(numofimages))
+        # Create dataset out of the 2 files:
+        self.data = tf.data.Dataset.from_tensor_slices(
+            (images_name_tensor, mask_name_tensor))
 
-	def next_batch(self, batch_size):
-		# This batch method chooses k data point from n without replacement
-		# If not enough input left in the "unused" list, refill
-		if(len(self.batch_list) <batch_size):
-			self.batch_list = np.array(range(len(self.input)))
-		# Choose k point from the list
-		idx = (np.random.choice(self.batch_list, batch_size,replace=False))
-		self.batch_list = np.setdiff1d(self.batch_list,idx)
-		# Return iterative object
-		yield self.input[idx], self.y[idx]
+        self.data = self.data.map(
+            lambda image_path, depthmap_path: tuple(tf.py_func(
+                _parse_depthmap, [image_path, depthmap_path], [image_path.dtype, tf.float32])))
+
+        # Parse images
+        self.data = self.data.map(
+            _parse_image, num_parallel_calls=self.num_threads).prefetch(30)
+
+        # If augmentation is to be applied
+        if self.augment:
+            self.data = self.data.map(_flip_left_right,
+                            num_parallel_calls=self.num_threads).prefetch(30)
+
+        # Batch the data
+        self.data = self.data.batch(self.batch_size)
+
+        # Resize to smaller dims for speed
+        self.data = self.data.map(_resize_data, num_parallel_calls=self.num_threads).prefetch(30)
+
+        # Normalize
+        self.data = self.data.map(_normalize_data,
+                        num_parallel_calls=self.num_threads).prefetch(30)
+
+        self.data = self.data.shuffle(30)
+
+        # Create iterator
+        self.iterator = tf.data.Iterator.from_structure(
+            self.data.output_types, self.data.output_shapes)
+
+        # Data set init. op
+        self.iter_init_op = self.iterator.make_initializer(self.data)
+        self.x, self.y = self.iterator.get_next()
 
--- codes/trainers/depth_estimator_trainer.py
+++ codes/trainers/depth_estimator_trainer.py
@@ -4,45 +4,42 @@ import numpy as np
 
 
 class DepthTrainer(BaseTrain):
-	def __init__(self, sess, model, data, config, logger):
-		super(DepthTrainer, self).__init__(sess, model, data, config, logger)
+    def __init__(self, sess, model, data, config, logger):
+        super(DepthTrainer, self).__init__(sess, model, data, config, logger)
 
-	def train_epoch(self):
-		# This function implements the operation in one epoch
-		
-		# Create an iterator 
-		loop = tqdm(range(self.config.num_iter_per_epoch))
-		lose_list = []
-		acc_list = []
+    def train_epoch(self):
+        # This function implements the operation in one epoch
 
-		# Loop for the batch 
-		for _ in loop:
-			loss,acc = self.train_step()
-			lose_list.append(loss)
-			acc_list.append(acc)
-		
-		loss = np.mean(lose_list)
-		acc = np.mean(acc_list)
+        self.sess.run(self.data.iter_init_op)
+        # Create an iterator
+        loop = tqdm(range(self.config.num_iter_per_epoch))
+        lose_list = []
+        acc_list = []
 
-		# Epoch summary
-		cur_it = self.model.global_step_tensor.eval(self.sess)
+        # Loop for the batch
+        for _ in loop:
+            loss, acc = self.train_step()
+            lose_list.append(loss)
+            acc_list.append(acc)
 
-		# For observing additional parameters, it is enough to add it to summaries_dict with a proper tag
-		summaries_dict = {
-			'loss': loss,
-			#'acc': acc,
-		}
-		self.logger.summarize(cur_it, summaries_dict=summaries_dict)
-		# Model is saved at the end of epoch
-		self.model.save(self.sess)
+        loss = np.mean(lose_list)
+        acc = np.mean(acc_list)
 
-	def train_step(self):
-		# Next batch returns a generator object(iterator). Next function takes the next input from this object
-		batch_x, batch_y = next(self.data.next_batch(self.config.batch_size))
-		feed_dict = {self.model.x: batch_x, self.model.y: batch_y, self.model.is_training: True}
+        # Epoch summary
+        cur_it = self.model.global_step_tensor.eval(self.sess)
 
-		_, loss, acc = self.sess.run([self.model.train_step, self.model.mse, self.model.accuracy],
-									 feed_dict=feed_dict)
-		
-		
-		return loss, acc
+        # For observing additional parameters, it is enough to add it to summaries_dict with a proper tag
+        summaries_dict = {
+            'loss': loss,
+            # 'acc': acc,
+        }
+        self.logger.summarize(cur_it, summaries_dict=summaries_dict)
+        # Model is saved at the end of epoch
+        self.model.save(self.sess)
+
+    def train_step(self):
+        # Next batch returns a generator object(iterator). Next function takes the next input from this object
+        feed_dict = {self.model.is_training: True}
+        _, loss, acc = self.sess.run([self.model.train_step, self.model.loss_function, self.model.accuracy],
+                                     feed_dict=feed_dict)
+        return loss, acc
